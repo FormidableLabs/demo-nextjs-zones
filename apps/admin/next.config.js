0232dec6561a39a8e291cff37726bb9bf92099f4
JonDavid Ebadirad <jondavid.ebadirad@formidable.com>
2022-08-03 11:08:31 -0700

    added themes

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -1,7 +1,9 @@
+const withTM = require("next-transpile-modules")(["@mfezones/ui"]);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withTM(nextConfig);
--- apps/admin/pages/_app.tsx
+++ apps/admin/pages/_app.tsx
@@ -1,8 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { themes, ThemeProvider, CssBaseLine, Paper } from "@mfezones/ui";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={themes.admin}>
+      <CssBaseLine />
+      <Paper elevation={0}>
+        <Component {...pageProps} />
+      </Paper>
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
